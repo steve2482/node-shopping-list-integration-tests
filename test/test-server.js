666aaa743413c66004cc5a5436c461ad71f78236
steve2482 <stevecb12684@yahoo.com>
2017-01-01 15:24:49 -0500

    added tests for GET, POST, PUT, and DELETE on /recipies endpoint

--- test/test-server.js
+++ test/test-server.js
@@ -142,3 +142,109 @@ describe('Shopping List', function() {
       });
   });
 });
+
+describe('Recipes', function () {
+  before(function() {
+    return runServer();
+  });
+  after(function() {
+    return closeServer();
+  });
+  // test strategy:
+  // 1. Make a GET request to '/recipes'
+  // 2. Inspect response object and that it has the correct key:value
+  // pairs and that the object has an 'id'
+  it('should list recipe items on GET', function() {
+    return chai.request(app)
+    .get('/recipes')
+    .then(function(res) {
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('array');
+      res.body.length.should.be.at.least(1);
+      const expectedKeys = ['id', 'name', 'ingredients'];
+      res.body.forEach(function(item) {
+        item.should.be.a('object');
+        item.should.include.keys(expectedKeys);
+      });
+    });
+  });
+
+  // test strategy:
+  // 1. Make a POST request using a example recipe
+  // 2. Inspect the response object and that it has the correct
+  // key:value pairs
+  it('should POST a new recipe and return that recipe', function() {
+    const newRecipe = {name: 'Macaroni & Cheese',ingredients: 'Macaroni, Milk, Cheese, Breadcrumbs'};
+    return chai.request(app)
+    .post('/recipes')
+    .send(newRecipe)
+    .then(function(res) {
+      res.should.have.status(201);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      const expectedKeys = ['id', 'name', 'ingredients'];
+      res.body.should.include.keys(expectedKeys);
+      res.body.id.should.not.be.null;
+      res.body.should.deep.equal(Object.assign(newRecipe, {id: res.body.id}));    
+    });
+  });
+
+  // test strategy:
+  //  1. initialize some update data (we won't have an `id` yet)
+  //  2. make a GET request so we can get an item to update
+  //  3. add the `id` to `updateData`
+  //  4. Make a PUT request with `updateData`
+  //  5. Inspect the response object to ensure it
+  //  has right status code and that we get back an updated
+  //  item with the right data in it.
+  it('should updated a current recipe', function() {
+    // we initialize our updateData here and then after the initial
+    // request to the app, we update it with an `id` property so
+    // we can make a second, PUT call to the app.
+    const updatedRecipe = {
+      name: 'foo',
+      ingredients: ['bar', 'bizz', 'buzz']
+    }
+    return chai.request(app)
+    // first have to get so we have an idea of object to update
+    .get('/recipes')
+    .then(function(res) {
+      updatedRecipe.id = res.body[0].id;
+      // this will return a promise whose value will be the response
+      // object, which we can inspect in the next `then` back. Note
+      // that we could have used a nested callback here instead of
+      // returning a promise and chaining with `then`, but we find
+      // this approach cleaner and easier to read and reason about.
+      return chai.request(app)
+      .put(`/recipes/${updatedRecipe.id}`)
+      .send(updatedRecipe);
+    })
+    // prove that the PUT request has right status code
+    // and returns updated item
+    .then(function(res) {
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      res.body.should.deep.equal(updatedRecipe);
+    });
+  });
+
+  // test strategy:
+  //  1. GET a shopping list items so we can get ID of one
+  //  to delete.
+  //  2. DELETE an item and ensure we get back a status 204
+  it('should delete recipes on DELETE', function() {
+    return chai.request(app)
+    // first have to get so we have an `id` of item
+    // to delete
+    .get('/recipes')
+    .then(function(res) {
+      return chai.request(app)
+        .delete(`/recipes/${res.body[0].id}`);
+    })
+    .then(function(res) {
+      res.should.have.status(204);
+    });
+  });  
+});
